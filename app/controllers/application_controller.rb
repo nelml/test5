568dbae7fcc36e4a62f0918d91c4d901c4167bec
nelml <konpaku.youmi@gmail.com>
2020-07-26 20:20:56 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path_for(resource)
+		user_path(resource)
+	end
+
+	def after_sign_up_path_for(resource)
+		user_path(resource)
+	end
+
+	protected
+
+
+	def configure_permitted_parameters
+		added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
+		#added_bttrs = [:name, :password, :remember_me]
+		devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password, :remember_me])
+	end
+end
